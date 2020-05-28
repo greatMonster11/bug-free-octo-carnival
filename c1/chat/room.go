2f8331412814d6d6c63b332449e4e153dbe4fd19
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-05-28 14:35:43 +0700

    turn room into HTTP handler while using concurrency

--- c1/chat/room.go
+++ c1/chat/room.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	// forward is a chanel that holds incomming message
 	// that should be forwarded to the other clients
@@ -10,4 +17,49 @@ type room struct {
 	leave chan *client
 	// clients hold add curren in the room
 	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
+}
+
+const (
+	socketBuffersize  = 1024
+	messageBuffersize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBuffersize, WriteBufferSize: messageBuffersize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP: ", err)
+		return
+	}
+
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBuffersize),
+		room:   r,
+	}
+
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
 }
