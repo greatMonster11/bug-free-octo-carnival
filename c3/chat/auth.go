3a4a2093496d6901eb4e2b5f7a16c77b43f13220
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-06-02 23:02:32 +0700

    add logout for user by removing cookie

--- c3/chat/auth.go
+++ c3/chat/auth.go
@@ -14,8 +14,8 @@ type authHandler struct {
 }
 
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_, err := r.Cookie("auth")
-	if err == http.ErrNoCookie {
+	cookie, err := r.Cookie("auth")
+	if err == http.ErrNoCookie || cookie.Value == "" {
 		// not authenticated
 		w.Header().Set("Location", "/login")
 		w.WriteHeader(http.StatusTemporaryRedirect)
