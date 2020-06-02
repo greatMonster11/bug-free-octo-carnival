f1c5c90aa95cf709dc290a3c1bfa046496b6fc59
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-06-02 22:36:48 +0700

    initial for c3

---
+++ c3/chat/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+
+	"github.com/greatMonster11/bug-free-octo-carnival/c1/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+)
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
+	})
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
+}
+
+func main() {
+	var addr = flag.String("host", ":8080", "The addr of the application.")
+	flag.Parse() // parse the flags
+
+	// setup gomniauth
+	gomniauth.SetSecurityKey("fafrf93jr98htuq34htg87htq8u2htq82hq2u")
+	gomniauth.WithProviders(
+		facebook.New("247468036676793", "c4ab86489ce441b2bf6db290ebf81895", "http://localhost:8080/auth/callback/facebook"),
+		github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
+		google.New("653373243882-vjaag2t8ppgnpevhpcuso9bjemv27abm.apps.googleusercontent.com", "_LYRgdIVRaEYs2zQ138z5sFa", "http://localhost:8080/auth/callback/google"),
+	)
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
+	// start the web server
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal(err)
+	}
+}
