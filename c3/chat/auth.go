d84ee98800ed88cdb6a3530670639d96475bcade
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-06-02 22:42:08 +0700

    transmitting the avatar URL

--- c3/chat/auth.go
+++ c3/chat/auth.go
@@ -76,7 +76,8 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		// parse RawQuery from the request
 		authCookieValue := objx.New(map[string]interface{}{
-			"name": user.Name(),
+			"name":       user.Name(),
+			"avatar_url": user.AvatarURL(),
 		}).MustBase64()
 
 		http.SetCookie(w, &http.Cookie{
