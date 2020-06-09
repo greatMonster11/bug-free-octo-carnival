092207c663ac9bb922a4d4853c4cee376389b86b
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-06-09 15:44:59 +0700

    add simple sprinkle for execute input command

---
+++ c4/sprinkle/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+const otherWord = "*"
+
+var transform = []string{
+	otherWord,
+	otherWord + "app",
+	otherWord + "site",
+	otherWord + "time",
+	"get" + otherWord,
+	"go" + otherWord,
+	"lets " + otherWord,
+	otherWord + "hq",
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		t := transform[rand.Intn(len(transform))]
+		fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
+	}
+}
